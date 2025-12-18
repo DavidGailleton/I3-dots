54d50adacf850dab7ecf2dd258be098239646008
David Gailleton <dgaillet@z4r10p7.42lyon.fr>
2025-12-18 21:17:17 +0000

    import .config files

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    alias ff=fastfetch
+    alias blue=blue42
+    alias c=clear
+    alias q=exit
+    starship init fish | source
+end
